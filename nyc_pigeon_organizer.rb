b907616663027372c1c9ab6b721ac3bdb7c57dcb
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-08 20:47:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,9 +11,9 @@ def nyc_pigeon_organizer(data)
   #hash with symbols color, gender, and lives
   details_array = []
   # Array will include actual colors, which gender, and where they live
-  data.collect do |description, details|
-    details.collect do |details, names|
-      names.collect do |names, i|
+  data.each do |description, details|
+    details.each do |details, names|
+      names.each do |names, i|
         if !new_hash[names]
           new_hash[names] = {}
         end
