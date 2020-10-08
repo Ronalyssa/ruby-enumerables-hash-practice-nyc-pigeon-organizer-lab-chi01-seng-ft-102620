df93769a89292247ac73496dededa822be57df63
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-08 20:11:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+#create empty hash first
+# loop to lowest nested level (triple loop)
+# conditional to be if present already do not add again if not add
 def nyc_pigeon_organizer(data)
   # write your code here!
+  i = 0
+  new_hash = {}
+  #overall main hash
+  description_key = {}
+  #hash with symbols color, gender, and lives
+  details_array = []
+  # Array will include actual colors, which gender, and where they live
+  data.collect do |description, details|
+    details.each do |details, names|
+      names.each do |names, i|
+        new_hash[names] = {}
+          new_hash[names][description] = []
+          new_hash[names][description] << details.to_s
+      end
+
+        binding.pry
+    end
+        new_hash
+        binding.pry
+  end
 end
